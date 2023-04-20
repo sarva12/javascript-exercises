ae326a6aaf21ac8b895c6d45771d47a1071b051d
sarvajith <sarvajithkr@gmail.com>
2023-04-20 19:22:26 -0400

    I added a repeat function when you add two different values to a named function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+    return str.repeat(times);
 };
 
-// Do not edit below this line
+let repeatedString = 'hey';
+let numberOfTimes = 3;
+
+console.log(repeatString(repeatedString, numberOfTimes));
+
 module.exports = repeatString;
